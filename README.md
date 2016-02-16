95804c4b58e21106181343941b7fd9668dab3de2
NicO'Sullivan <nic.osullivan@eagles.oc.edu>
2016-02-16 17:11:37 -0600

    added error checking on parameter and actually quit when wrong number of parameters encountered, also moved the check vote logic to a function

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 ## vote-sim
 
-This is a program for OC's computer science to help find out if it is feasible for a democrat to run for office in a district that the Dem/Rep ratio is 2:1. It will calculate how many people will vote and what they will vote, based on whether or not the candidate encountered them.
+This is a program for OC's Computer Simulation class to help find out if it is feasible for a democrat to run for office in a district that the Dem/Rep ratio is 2:1. It will calculate how many people will vote and what they will vote, based on whether or not the candidate encountered them.
 
 ### Parameters
 1. Population of district
 
-- Number of days spent meeting people
+2. Number of days spent meeting people
 
-- Number of people meet per day.
+3. Number of people meet per day.
--- main.go
+++ main.go
@@ -18,13 +18,23 @@ func main() {
   // Check to ensure the right number of arguments
   if len(os.Args) != 4 {
     fmt.Println("Usage: vote-sim population days encounter-per-day")
+    os.Exit(1)
   }
 
   // Get arguments from command line
   population, err := strconv.Atoi(os.Args[1])
+  // Check if error occured
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
   days, err := strconv.Atoi(os.Args[2])
+  // Check if error occured
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
   rate, err := strconv.Atoi(os.Args[3])
-
   // Check if error occured
   if err != nil {
     fmt.Println(err)
@@ -43,72 +53,69 @@ func main() {
   var demVotes int
   var repVotes int
 
+  // Seed random number with time
+  seed := rand.NewSource(time.Now().UnixNano())
+  ran := rand.New(seed)
+
   // Simulate one day at a time
   for i := 0; i < days; i++ {
     for j := 0; j < rate; j++ {
       total++
-      seed := rand.NewSource(time.Now().UnixNano())
-      ran := rand.New(seed)
-      num := ran.Intn(100)
+      party := ran.Intn(100)
       voteChance := ran.Intn(100)
       voteSide := rand.Intn(100)
       // Check what party they are in
-      if num < partyLine {  //Democrat
-        if voteChance < demEncounter.VoteChance {
-          if voteSide < demEncounter.ChanceRepublican {
-            repVotes++
-          } else {
-            demVotes++
-          }
-        }
-      } else {          //Republican
-        if voteChance < repEncounter.VoteChance {
-          if voteSide < repEncounter.ChanceRepublican {
-            repVotes++
-          } else {
-            demVotes++
-          }
-        }
+      if party < partyLine {  //Democrat
+        r, d := calcVote(demEncounter, voteChance, voteSide)
+        repVotes += r
+        demVotes += d
+      } else {    //Republican
+        r, d := calcVote(repEncounter, voteChance, voteSide)
+        repVotes += r
+        demVotes += d
       }
     }
   } // End Days loop
 
-
   // Calculate how the rest of the population votes(or doesn't)
   for k := total; k < population; k++ {
     total++
-    seed := rand.NewSource(time.Now().UnixNano())
-    ran := rand.New(seed)
     party := ran.Intn(100)
     voteChance := ran.Intn(100)
     voteSide := rand.Intn(100)
 
     // Check what party they are in
     if party < partyLine {  // Democrat
-      if voteChance < democrat.VoteChance {
-        if voteSide < democrat.ChanceRepublican {
-          repVotes++
-        } else {
-          demVotes++
-        }
-      }
+      r, d := calcVote(democrat, voteChance, voteSide)
+      repVotes += r
+      demVotes += d
     } else {    // Republican
-      if voteChance < republican.VoteChance {
-        if voteSide < republican.ChanceRepublican {
-          repVotes++
-        } else {
-          demVotes++
-        }
-      }
+      r, d := calcVote(republican, voteChance, voteSide)
+      repVotes += r
+      demVotes += d
     }
   }
-
 
   fmt.Println("Total: ", total)
   fmt.Println("Total Votes: ", demVotes + repVotes)
   fmt.Println("No Votes: ", total-(demVotes+repVotes))
   fmt.Println("Vote Dem: ", demVotes)
   fmt.Println("Vote Rep: ", repVotes)
+  fmt.Println("Difference", demVotes-repVotes)
+  if demVotes - repVotes > 0 {
+    fmt.Println("WIN!")
+  } else {
+    fmt.Println("loss")
+  }
+}
 
-
+func calcVote(vote VoteStats, chance, side int) (rep, dem int) {
+  if chance < vote.VoteChance {
+    if side < vote.ChanceRepublican {
+      rep++
+    } else {
+      dem++
+    }
+  }
+  return
 }
